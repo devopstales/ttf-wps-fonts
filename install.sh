dd6c9cbd77d3cd5272857eee459169f543a01e70
devopstales <devopstales@protonmail.com>
2020-01-29 19:54:37 +0100

    add more fonts

--- install.sh
+++ install.sh
@@ -51,6 +51,20 @@ fi
 echo "Installing Fonts..."
 cp *.ttf $FONT_PATH
 cp *.TTF $FONT_PATH
+
+sudo dnf install -y ipa-gothic-fonts ipa-pgothic-fonts ipa-mincho-fonts ipa-pmincho-fonts
+sudo dnf install -y thai-scalable-garuda-fonts
+sudo dnf install -y dejavu-sans-fonts
+
+cd /tmp
+wget http://download.savannah.nongnu.org/releases/freebangfont/MuktiNarrow-0.94.tar.bz2
+tar -xjf MuktiNarrow-0.94.tar.bz2
+sudo mv MuktiNarrow0.94/MuktiNarrow.ttf /usr/share/fonts/truetype/ttf-indic-fonts-core/
+rm MuktiNarrow-0.94.tar.bz2
+rm -r MuktiNarrow0.94/
+
+sudo dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
 echo "Fixing Permissions..."
 chmod 644 $FONT_PATH/*
 echo "Rebuilding Font Cache..."
